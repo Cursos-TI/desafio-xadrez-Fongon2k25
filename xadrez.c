4c98ec7acf7e2763e540c808b8f7b3a5d5b3b88a
Fongon2k25 <junior.priscilla09@gmail.com>
2025-04-03 17:59:29 +0000

    implementando a recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
-
-int main() {
-    int torre, rainha = 0, bispo = 1;
-
-    while (bispo <= 5) {
-        printf("Bispo movendo em diagonal (cima, direita).\n");
-        bispo++;
+// movendo torre 5 casas
+void movendotorre (int casas){
+    if (casas > 0){
+        printf("Torre movendo para Direita.\n");
+        movendotorre(casas - 1);
     }
-
-    for (torre = 1; torre <= 5; torre++) {
-        printf("Torre movendo para direita.\n");
+}
+// movendo rainha 8 casas 
+void movendorainha (int casas){
+    if (casas > 0){
+        printf("Rainha movendo para esquerda.\n");
+        movendorainha(casas - 1);
     }
-
-    {
-        do {
-            printf("Rainha movendo para esquerda.\n");
-            rainha++;
-        } while (rainha < 8);
+}
+// movendo bispo 5 casas 
+void movendobispo (int casas){
+    if (casas > 0){
+        printf("Movendo bispo, diagonal(Direita,cima).\n");
+        movendobispo(casas - 1);
     }
-
-    {
-        for (int cavalo = 2; cavalo <= 1; cavalo--) {
-            // Este loop não executará pois a condição é falsa
-        }
-
-        for (int i = 0; i < 2; i++) {
-            printf("Cavalo movendo para baixo\n");
-        }
-
-        printf("Cavalo movendo para esquerda\n");
-    }
+}
 
 
 
 
 
+int main (){
+
+movendotorre (5);
+movendorainha(8);
+movendobispo(5);
+// movendo cavalo 
+for (int cavalo = 2; cavalo <= 1; cavalo--)
+{
+
+}
+
+{
+
+    for(int i = 0; i < 2; i++){
+
+    printf("cavalo movendo para baixo\n");
+
+}
+
+printf ("Cavalo movendo para esquerda\n");
+
+
+}
+
+
+
+return 0;
 
 
 
@@ -43,6 +60,4 @@ int main() {
 
 
 
-
-    return 0;
 }
