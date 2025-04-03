8c517f1b0bfff2c099fb163b01f9c417167bdaef
Fongon2k25 <junior.priscilla09@gmail.com>
2025-04-03 15:57:54 +0000

    inclusao do cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
-int main (){
+int main() {
+    int torre, rainha = 0, bispo = 1;
 
-int torre, rainha, bispo = 1;
+    while (bispo <= 5) {
+        printf("Bispo movendo em diagonal (cima, direita).\n");
+        bispo++;
+    }
 
+    for (torre = 1; torre <= 5; torre++) {
+        printf("Torre movendo para direita.\n");
+    }
 
+    {
+        do {
+            printf("Rainha movendo para esquerda.\n");
+            rainha++;
+        } while (rainha < 8);
+    }
 
-while (bispo <= 5)
-{
-    printf ("bispo movendo em diagonal (cima, direita).\n");
-    bispo ++;
-}
+    {
+        for (int cavalo = 2; cavalo <= 1; cavalo--) {
+            // Este loop não executará pois a condição é falsa
+        }
 
-for (torre = 1; torre <= 5; torre++)
-{
-    printf("Torre movendo para direita.\n");
-}
+        for (int i = 0; i < 2; i++) {
+            printf("Cavalo movendo para baixo\n");
+        }
 
-do
-{
-    printf("Rainha movendo para esquerda.\n");
-    rainha ++;
-} while (rainha < 8);
+        printf("Cavalo movendo para esquerda\n");
+    }
 
 
 
